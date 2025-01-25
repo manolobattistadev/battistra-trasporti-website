f99502ef134087f122135f62df527c81e129371c
Manolo Battista <manolo@arkemis.it>
2025-01-25 14:56:08 +0100

    feat: optimize images

--- components/whatsapp-floating-btn.tsx
+++ components/whatsapp-floating-btn.tsx
@@ -4,7 +4,9 @@ export default function WhatsappFloatingBtn(){
     return (
         <div
             className="shadow-xl cursor-pointer fixed bottom-8 right-8 bg-black p-2 px-4 rounded-full text-white flex gap-2">
-            <a href="https://wa.me/+39335453733" className="flex gap-4" target="_blank">
+            <a
+                aria-label="Contattaci su whatsapp"
+                href="https://wa.me/+39335453733" className="flex gap-4" target="_blank">
                 <WhatsappIcon/>
             </a>
         </div>
